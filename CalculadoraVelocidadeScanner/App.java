10ffbcc8835ad5445e9ec9b764349a175c90c76f
Edu1Sinico <EduSenai888@gmail.com>
2023-02-13 16:25:19 -0200

    Aula 13/02/2023 - Scanner

---
+++ CalculadoraVelocidadeScanner/App.java
@@ -0,0 +1,8 @@
+package CalculadoraVelocidadeScanner;
+
+public class App {
+    public static void main(String[] args) {
+        calcularVelocidade cVelocidade = new calcularVelocidade();
+        cVelocidade.calcular();
+    }
+}

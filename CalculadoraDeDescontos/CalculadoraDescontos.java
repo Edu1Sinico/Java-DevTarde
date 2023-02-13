c8483ff7db2dc6f7bde1e660b92fbd16b2549129
Edu1Sinico <EduSenai888@gmail.com>
2023-02-13 14:54:13 -0200

    Aula 13/02/2023 - Classes e Métodos

--- CalculadoraDeDescontos/CalculadoraDescontos.java
+++ CalculadoraDeDescontos/CalculadoraDescontos.java
@@ -4,14 +4,27 @@ package CalculadoraDeDescontos;
  * CalculadoraDescontos
  */
 public class CalculadoraDescontos {
+  public void Calculadora20()
+    {
+        System.out.println("=======================================");
+        double precoProduto = 115, percentualDesconto = 20; 
+        System.out.println("O preço do produto é de " + precoProduto + " reais"); 
+        System.out.println("O desconto será de " + percentualDesconto + " %");
+        double desconto = precoProduto*percentualDesconto/100;
+        double total = precoProduto-desconto;
+        System.out.println("o preço do produto com o desconto: R$ " + total);
+        System.out.println("=======================================");
+    }
 
- System.out.println("=======================================");
- double precoProduto = 115, percentualDesconto = 10.0;
- System.out.println("O preço do produto é de " + prcoProduto + " reais");
- percentualDesconto = 20;
- System.out.println("O desconto será de " + percentualDesconto + " %");
- double desconto = precoProduto*percentualDesconto/100;
- double total = precoProduto-desconto;
- system.out.println("o preço do produto com o desconto: R$ " + total);
- system.out.println("=======================================");
+    public void Calculadora30()
+    {
+        System.out.println("=======================================");
+        double precoProduto = 115, percentualDesconto = 30; 
+        System.out.println("O preço do produto é de " + precoProduto + " reais"); 
+        System.out.println("O desconto será de " + percentualDesconto + " %");
+        double desconto = precoProduto*percentualDesconto/100;
+        double total = precoProduto-desconto;
+        System.out.println("o preço do produto com o desconto: R$ " + total);
+        System.out.println("=======================================");
+    }
 }
--- CalculadoraDeDescontos/Main.java
+++ CalculadoraDeDescontos/Main.java
@@ -1,7 +1,9 @@
 package CalculadoraDeDescontos;
 
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        
+        CalculadoraDescontos cDescontos = new CalculadoraDescontos();
+        cDescontos.Calculadora20();
+        cDescontos.Calculadora30();
     }
 }

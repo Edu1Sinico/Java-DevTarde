c2f9684d68aa980b5590f117719df07528d1661c
Edu1Sinico <EduardoSenai888@gmail.com>
2023-03-08 10:18:21 -0300

    Exercicios IFElse Java "06/03/2023"

---
+++ ExercicioIFElseJava/ClasseDecisao.java
@@ -0,0 +1,184 @@
+package ExercicioIFElseJava;
+
+import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner14;
+
+public class ClasseDecisao {
+int x = 0, y = 0, z = 0, total = 0;
+double total1;
+Scanner sc = new Scanner(System.in);
+
+    //Método do exercício 1
+    public void Exercicio1(){
+        System.out.println("Digite o primeiro número: ");
+        x = sc.nextInt();
+        System.out.println("Agora digite o segundo número: ");
+        y = sc.nextInt();
+        if (x != y){
+            if (x > y)
+            {
+                System.out.println("O primeiro número digitado '"+ x +"' é maior que o segundo '" + y +"'.");
+            }
+            else{
+                System.out.println("O segundo número digitado '"+ y +"' é maior que o primeiro '" + x + "'.");
+            }
+        }
+        else{
+            System.out.println("Os números digitados são iguais.");
+        }
+    }
+
+    //Método do exercício 2
+    public void Exercicio2(){
+        System.out.println("Digite o ano de seu nascimento: ");
+        x = sc.nextInt();
+        total = 2022 - x;
+        if (total >= 16)
+        {
+            System.out.println("Você pode votar.");
+        }
+        else{
+            System.out.println("Você não poderá votar esse ano.");
+        }
+    }
+
+    //Método do exercício 3
+    public void Exercicio3(){
+        System.out.println("Digite a senha: ");
+        x = sc.nextInt();
+        if (x == 1234)
+        {
+            System.out.println("Access granted.");
+        }
+        else{
+            System.out.println("Access denied.");
+        }
+    }
+
+    //Método do exercício 4
+    public void Exercicio4(){
+        System.out.println("Digite a quantidade de maçãs coletadas: ");
+        double x = sc.nextInt();
+        if (x < 12)
+        {
+            total1 = x*0.30;
+            System.out.println("As maçãs irão custar: " + total1);
+        }
+        else if (x >= 12)
+        {
+            total1 = x*0.25;
+            System.out.println("As maçãs irão custar: " + total1);
+        }
+    }
+
+    //Método do exercício 5
+    public void Exercicio5(){
+        System.out.println("Digite o 1º número: ");
+        x = sc.nextInt();
+        System.out.println("Digite o 2º número: ");
+        y = sc.nextInt();
+        System.out.println("Digite o 3º número: ");
+        z = sc.nextInt();
+        if (x != y && x != z && y != z){
+            if(x > y && x > z && z < y)
+            {
+                System.out.println("Ordem crescente:");
+                System.out.println("1º " + z);
+                System.out.println("2º " + y);
+                System.out.println("3º " + x);
+            }
+            else if(y > x && y > z && z < x)
+            {
+                System.out.println("Ordem crescente:");
+                System.out.println("1º " + z);
+                System.out.println("2º " + x);
+                System.out.println("3º " + y);
+            }
+            else if(z > x && z > y && y < x)
+            {
+                System.out.println("Ordem crescente:");
+                System.out.println("1º " + y);
+                System.out.println("2º " + x);
+                System.out.println("3º " + z);
+            }
+            //-----------------------------------------------
+            else if(x > y && x > z && y < z)
+            {
+                System.out.println("Ordem crescente:");
+                System.out.println("1º " + y);
+                System.out.println("2º " + z);
+                System.out.println("3º " + x);
+            }
+            else if(y > x && y > z && x < z)
+            {
+                System.out.println("Ordem crescente:");
+                System.out.println("1º " + x);
+                System.out.println("2º " + z);
+                System.out.println("3º " + y);
+            }
+            else if(z > x && z > y && x < y)
+            {
+                System.out.println("Ordem crescente:");
+                System.out.println("1º " + x);
+                System.out.println("2º " + y);
+                System.out.println("3º " + z);
+            }
+        }
+        else{
+            System.out.println("Os números digitados são iguais.");
+        }
+    }
+
+    public void Exercicio6(){
+        System.out.println("Digite sua altura: ");
+        double altura = sc.nextDouble();
+        System.out.println("Precione 1 para o sexo masculino ou 2 para o sexo feminino.");
+        int sexo = sc.nextInt();
+        if (sexo == 1)
+        {
+            total1 = (72.7*altura)-58;
+            System.out.println("O peso ideal é: " + total1);
+        }
+        else if (sexo == 2)
+        {
+            total1 = (62.1*altura)-44.7;
+            System.out.println("O peso ideal é: " + total1);
+        }
+        else
+        System.out.println("Precione um desses números.");
+    }
+
+    //Exercicio7
+    public void Exercicio7(){
+        System.out.println("Informe a nota 1 do aluno: ");
+        x = sc.nextInt();
+        System.out.println("Informe a nota 2 do aluno: ");
+        y = sc.nextInt();
+        double media = (x+y)/2;
+        System.out.println("A média do aluno é: " + media);
+        if (media >= 50)
+        {
+            System.out.println("Aluno aprovado por nota.");
+        }
+        else{
+            System.out.println("Aluno reprovado por nota.");
+        }
+        System.out.println("Informe a frequência do aluno: ");
+        int freqAluno = sc.nextInt();
+        if (freqAluno >= 75)
+        {
+            System.out.println("Aluno aprovado por frequência.");
+        }
+        else{
+            System.out.println("Aluno reprovado por frequência.");
+        }
+        if (media >= 50 && freqAluno >= 75)
+        {
+            System.out.println("Aluno aprovado no curso.");
+        }
+        else{
+            System.out.println("Aluno reprovado no curso.");
+        }
+    }
+}
---
+++ ExercicioIFElseJava/main.java
@@ -0,0 +1,63 @@
+package ExercicioIFElseJava;
+
+import java.util.Scanner;
+
+import ClasseDecisao;
+
+public class main {
+    public static void main(String[] args) {
+        ClasseDecisao cd = new ClasseDecisao();
+        Scanner sc = new Scanner(System.in);
+        int receber = 0, continuar;
+        do {
+            System.out.println("Exercício de estruturad de decisão.");
+            System.out.println("========================================");
+            System.out.println("Precione 1 para o primeiro exercício.");
+            System.out.println("Precione 2 para o segundo exercício.");
+            System.out.println("Precione 3 para o terceiro exercício.");
+            System.out.println("Precione 4 para o quarto exercício.");
+            System.out.println("Precione 5 para o quinto exercício.");
+            System.out.println("Precione 6 para o sexto exercício.");
+            System.out.println("Precione 7 para o sétimo exercício.");
+            System.out.println("");
+            receber = sc.nextInt();
+
+            switch (receber) {
+                case 1: {
+                    cd.Exercicio1();
+                    break;
+                }
+                case 2: {
+                    cd.Exercicio2();
+                    break;
+                }
+                case 3: {
+                    cd.Exercicio3();
+                    break;
+                }
+                case 4: {
+                    cd.Exercicio4();
+                    break;
+                }
+                case 5: {
+                    cd.Exercicio5();
+                    break;
+                }
+                case 6: {
+                    cd.Exercicio6();
+                    break;
+                }
+                case 7: {
+                    cd.Exercicio7();
+                    break;
+                }
+                default: {
+                    System.out.println("Por favor, digite um dos números pedidos acima.");
+                    break;
+                }
+            }
+            System.out.println("Deseja continuar? Precione 0, caso contrario, precione qualquer outro número.");
+            continuar = sc.nextInt();
+        } while (continuar == 0);
+    }
+}

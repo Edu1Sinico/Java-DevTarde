7a42b74ed3bf6e8d1d619f0cbe7f247b91f30e51
Edu1Sinico <EduardoSenai888@gmail.com>
2023-04-15 17:28:17 -0300

    Exercicios de iteração com For

--- ExerciciosIteracoesForWhile/ExercicioIteracoes.java
+++ ExerciciosIteracoesForWhile/ExercicioIteracoes.java
@@ -5,50 +5,229 @@ import java.util.Scanner;
 public class ExercicioIteracoes {
     Scanner sc = new Scanner(System.in);
 
+    // -----------------------------------------------------------------------------------------
+    // Método 1
     public void Metodo01() {
         int[] vetor = new int[5];
 
-        for(int i = 0; i < vetor.length; i++)
-        {
-            System.out.println("Informe para o vetor de índice "+i+": ");
-            vetor[i] = sc.nextInt();      
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println("Informe para o vetor de índice " + (i+1) + ": ");
+            vetor[i] = sc.nextInt();
         }
 
-        for(int i = 0; i < vetor.length; i++)
-        {
-            System.out.println("O elemento do vetor no índice "+i+" é:" + vetor[i]);
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println("O elemento do vetor no índice " + (i+1) + " é:" + vetor[i]);
         }
     }
 
+    // -----------------------------------------------------------------------------------------
+    // Método 2
     public void Metodo02() {
-        int[] vetor = new int[10];
+        double[] vetor = new double[10];
+
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println("Informe para o vetor de índice " + (i+1) + ": ");
+            vetor[i] = sc.nextDouble();
+        }
+        for (int i = 9; i < vetor.length; i--) {
+            System.out.println("O elemento do vetor no índice " + (i+1) + " é:" + vetor[i]);
+        }
     }
 
+    // -----------------------------------------------------------------------------------------
+    // Método 3
     public void Metodo03() {
-        int[] vetor = new int[4];
+        double[] vetor = new double[4];
+        double nota = 0, total = 0, media = 0;
+
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println("Informe a " + (i+1) + "ª nota do aluno: ");
+            vetor[i] = sc.nextDouble();
+        }
+
+        for (int i = 0; i < vetor.length; i++) {
+            nota = vetor[i];
+            total += nota;
+        }
+        media = total / 4;
+        for (int i = 0; i < vetor.length; i++) {
+            j++;
+            System.out.println("A " + (i+1) + "ª nota digita foi: " + vetor[i]);
+        }
+        System.out.println("A média será de: " + media);
+
     }
 
+    // -----------------------------------------------------------------------------------------
+    // Método 4
     public void Metodo04() {
-        
+        char[] vetorLetras = new char[10];
+        int consoantes = 0;
+
+        System.out.println("Digite 10 caracteres: ");
+        for (int i = 0; i < vetorLetras.length; i++) {
+            System.out.print((i+1) + "º ");
+            vetorLetras[i] = sc.next().charAt(0);
+            if (Character.isLetter(vetorLetras[i]) && !Vogal(vetorLetras[i])) {
+                consoantes++;
+            }
+        }
+
+        System.out.println("Número de consoantes lidas: " + consoantes);
+        System.out.println("Consoantes lidas:");
+        for (char c : vetorLetras) {
+            if (Character.isLetter(c) && !Vogal(c)) {
+                System.out.println(c);
+            }
+        }
+
     }
 
+    public static boolean Vogal(char c) {
+        c = Character.toLowerCase(c);
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
+    // -----------------------------------------------------------------------------------------
+    // Método 5
     public void Metodo05() {
-        
+        int[] vetor = new int[20];
+        int[] vetorPar = new int[20];
+        int[] vetorImpar = new int[20];
+
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println("Informe para o vetor de índice " + (i+1) + ": ");
+            vetor[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < vetor.length; i++) {
+            if (vetor[i] % 2 == 0) {
+                vetorPar[i] = vetor[i];
+            } else {
+                vetorImpar[i] = vetor[i];
+            }
+        }
+
+        System.out.println("Números que você informou: ");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println((i+1) + "º " + vetor[i]);
+        }
+
+        System.out.println("");
+        System.out.println("Vetor de números pares: ");
+        for (int i = 0; i < vetorPar.length; i++) {
+            if (vetorPar[i] != 0)
+                System.out.println(vetorPar[i]);
+        }
+
+        System.out.println("");
+        System.out.println("Vetor de números impares: ");
+        for (int i = 0; i < vetorImpar.length; i++) {
+            if (vetorImpar[i] != 0)
+                System.out.println(vetorImpar[i]);
+        }
     }
 
+    // -----------------------------------------------------------------------------------------
+    // Método 6
     public void Metodo06() {
-        
+        double[][] notas = new double[10][4];
+        double[] medias = new double[10];
+        double[] nota = new double [4];
+        int alunos = 0;
+        double media = 0;
+
+        for (int i = 0; i < notas.length; i++)
+        {
+            System.out.println("Digite as quatro notas do aluno " + (i+1) + ":");
+            for (int j = 0; j < 4; j++) {
+                notas[i][j] = sc.nextDouble();
+                nota[i] += notas[i][j];
+            }
+            medias[i] = nota[i]/4;
+        }
+
+        for (int i = 0; i < medias.length; i++)
+        {
+            if (medias[i] >= 7) {
+                alunos++;
+            }  
+        }
+        System.out.println("Número de alunos com média maior ou igual a 7: " + alunos);
     }
 
+    // -----------------------------------------------------------------------------------------
+    // Método 7
     public void Metodo07() {
-        
+        int[] vetor = new int[5];
+        int[] vetorTotalSoma = new int[5];
+        int[] vetorTotalMult = new int[5];
+
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println("Informe para o vetor de índice " + (i+1) + ": ");
+            vetor[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < vetorTotalSoma.length; i++) {
+            vetorTotalSoma[i] = vetor[i] + vetor[i];
+        }
+
+        for (int i = 0; i < vetorTotalMult.length; i++) {
+            vetorTotalMult[i] = vetor[i] * vetor[i];
+        }
+
+        System.out.println("Os valores que você informou: ");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println((i+1) + "º " + vetor[i]);
+        }
+
+        System.out.println("");
+        System.out.println("A soma dos vetores ");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println((i+1) + "º " + vetorTotalSoma[i]);
+        }
+
+        System.out.println("");
+        System.out.println("A multiplicação dos vetores ");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println((i+1) + "º " + vetorTotalSoma[i]);
+        }
     }
 
+    // -----------------------------------------------------------------------------------------
+    // Método 8
     public void Metodo08() {
-        
+
     }
 
+    // -----------------------------------------------------------------------------------------
+    // Método 9
     public void Metodo09() {
-        
+        int[] vetor = new int[10];
+        int[] vetorQuad = new int[10];
+        int[] vetorTotal = new int[10];
+        int j = 0;
+
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println("Informe para o vetor de índice " + (i+1) + ": ");
+            vetor[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < vetor.length; i++)
+        {
+            vetorQuad[i] = (int) Math.pow(vetor[i], 2);
+        }
+
+        for (int i = 0; i < vetor.length; i++)
+        {
+            vetorTotal[i] = vetorQuad[i] + vetorQuad[i];
+        }
+
+        System.out.println("A soma dos quadrados para cada elemento será: ");
+        for (int i = 0; i < vetor.length; i++)
+        {
+            j++;
+            System.out.println(j + "º " + vetorTotal[i]);
+        }
     }
 }
--- ExerciciosIteracoesForWhile/Main.java
+++ ExerciciosIteracoesForWhile/Main.java
@@ -81,5 +81,4 @@ public class Main {
         } while (x == 0);
     }
 }
-    }
-}
+

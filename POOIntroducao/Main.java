19bc36dacce862aded366a0d69bf390aea8897b2
Edu Sinico <123770640+Edu1Sinico@users.noreply.github.com>
2023-05-15 18:41:14 +0000

    15/05/2023 - Introdução a Programação Orientada a Objeto

---
+++ POOIntroducao/Main.java
@@ -0,0 +1,55 @@
+package POOIntroducao;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // criar 3 objetos ResgistraAluno
+        RegistraAluno rAluno = new RegistraAluno();
+
+        System.out.println("Digite o nome do aluno(a): ");
+        String nome = sc.nextLine();
+        rAluno.setNome(nome);
+        System.out.println("");
+
+        System.out.println("Digite a idade do aluno(a): ");
+        int idade = sc.nextInt();
+        rAluno.setIdade(idade);
+        System.out.println("");
+
+        System.out.println("Digite o endereço do aluno(a): ");
+        sc.next();
+        String endereco = sc.nextLine();
+        rAluno.setEndereco(endereco);
+        System.out.println("");
+
+        System.out.println("Digite a nota de matemática do aluno(a): ");
+        double notaMat = sc.nextDouble();
+        rAluno.setNotaMatematica(notaMat);
+        System.out.println("");
+
+        System.out.println("Digite a nota de Língua Portuguesa do aluno(a): ");
+        double notaLP = sc.nextDouble();
+        rAluno.setNotaPortugues(notaLP);
+        System.out.println("");
+
+        System.out.println("Digite a nota de Ciências do aluno(a): ");
+        double notaCN = sc.nextDouble();
+        rAluno.setNotaCiencias(notaCN);
+        System.out.println("");
+
+        System.out.println("==================================");
+        System.out.println("");
+
+        System.out.println("Nome: " + rAluno.getNome());
+        System.out.println("");
+        System.out.println("Idade: " + rAluno.getIdade());
+        System.out.println("");
+        System.out.println("Endereço: " + rAluno.getEndereco());
+        System.out.println("");
+        System.out.println("Média do aluno(a):" + rAluno.getMedia());
+        System.out.println("");
+        System.out.println("Contador: " + RegistraAluno.getQuantidadeAlunos());
+    }
+}
---
+++ POOIntroducao/RegistraAluno.java
@@ -0,0 +1,63 @@
+package POOIntroducao;
+
+public class RegistraAluno {
+    private String nome;
+    private String endereco;
+    private int idade;
+    private double notaMatematica;
+    private double notaPortugues;
+    private double notaCiencias;
+    private static int contadorEstudante;
+
+    // getters
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getMedia() {
+        double resultado = 0;
+        resultado = (notaMatematica
+                + notaPortugues
+                + notaCiencias) / 3;
+
+        return resultado;
+    }
+
+    public static int getQuantidadeAlunos() {
+        return contadorEstudante;
+    }
+
+
+    // Setters
+public void setNome(String nome){
+    this.nome = nome;
+    }
+
+    public void setEndereco(String endereco){
+    this.endereco = endereco;
+    }
+
+    public void setIdade(int idade){
+    this.idade = idade;
+    }
+
+    public void setNotaMatematica(double notaMatematica){
+    this.notaMatematica = notaMatematica;
+    }
+
+    public void setNotaPortugues(double notaPortugues){
+    this.notaPortugues = notaPortugues;
+    }
+
+    public void setNotaCiencias(double notaCiencias){
+    this.notaCiencias = notaCiencias;
+    }
+}

589b22c042ab8b624623a0f75f9676c5913d2875
Edu1Sinico <EduardoSenai888@gmail.com>
2023-06-05 15:11:57 -0300

    Enviando a atividade corrigida para o GitHub

---
+++ ExerArray/Exercicio2/Agenda.java
@@ -0,0 +1,32 @@
+package ExerArray.Exercicio2;
+
+public class Agenda {
+    String nome, altura, idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    public String getAltura() {
+        return altura;
+    }
+
+    public void setAltura(String altura) {
+        this.altura = altura;
+    }
+
+    //getters e setters
+
+}
---
+++ ExerArray/Exercicio3/Elevador.java
@@ -0,0 +1,9 @@
+package ExerArray.Exercicio3;
+
+public class Elevador {
+    int andar = 0;
+
+    public void name() {
+        
+    }
+}

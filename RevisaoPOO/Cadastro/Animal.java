fa489ea71f1892a4e2f15eb11cc8c528c66648de
Edu1Sinico <EduardoSenai888@gmail.com>
2023-07-25 15:20:06 -0300

    Voltando as origens...

---
+++ RevisaoPOO/Cadastro/Animal.java
@@ -0,0 +1,66 @@
+package RevisaoPOO.Cadastro;
+
+public class Animal {
+    
+    //Atributos
+    String porte;
+    double peso;
+    String proprietario;
+    String genero;
+    String especie;
+
+    //métodos
+    //construtor cheio
+        public Animal(String porte, double peso, String proprietario, String genero, String especie) {
+        this.porte = porte;
+        this.peso = peso;
+        this.proprietario = proprietario;
+        this.genero = genero;
+        this.especie = especie;
+    }
+
+    // construtor vazio
+    public Animal(){
+    }
+
+    //Getters and Setters
+    public String getPorte() {
+        return porte;
+    }
+
+    public void setPorte(String porte) {
+        this.porte = porte;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public String getProprietario() {
+        return proprietario;
+    }
+
+    public void setProprietario(String proprietario) {
+        this.proprietario = proprietario;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+}
---
+++ RevisaoPOO/Main.java
@@ -0,0 +1,11 @@
+package RevisaoPOO;
+
+import RevisaoPOO.Cadastro.Animal;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Animal obj = new Animal("pequeno", 07.55, "Diogo", "Femêa", "Hamster");
+    }
+
+}

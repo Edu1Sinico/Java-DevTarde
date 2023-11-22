dacc192aa08bd99e594f8a633ae2fa545f6309f0
Edu1Sinico <EduardoSenai888@gmail.com>
2023-11-22 16:51:56 -0300

    Enviando o projeto quase pronto de java com banco de dados para o github

--- GUI/GUI_JDBC_Loja_Carros/src/Connection/RegistrarVendaDAO.java
+++ GUI/GUI_JDBC_Loja_Carros/src/Connection/RegistrarVendaDAO.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class RegistrarVendaDAO {
     // atributo
     private Connection connection;
-    private List<Carros> carros;
 
     // construtor
     public RegistrarVendaDAO() {
